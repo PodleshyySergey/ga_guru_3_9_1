82f718f72ceb6f33ac7370ace24f639f01b95b9c
PodlesnyySergey <serpodl@gmail.com>
2020-12-18 23:38:37 +0300

    Comment out Configuration.remote for Selenoid. Add class TestBase.

--- src/test/java/helpers/AttachmentsHelper.java
+++ src/test/java/helpers/AttachmentsHelper.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import tests.TestBase;
 
 import java.nio.charset.StandardCharsets;
 
--- src/test/java/tests/CheckFormTests.java
+++ src/test/java/tests/CheckFormTests.java
@@ -1,27 +1,20 @@
 package tests;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import com.github.javafaker.service.FakeValuesService;
 import com.github.javafaker.service.RandomService;
-import io.qameta.allure.Step;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import java.io.File;
 import java.util.Locale;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
-import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
-import static helpers.AttachmentsHelper.*;
 import static io.qameta.allure.Allure.step;
 
-import com.github.javafaker.Faker;
-
-public class CheckFormTests {
+public class CheckFormTests extends TestBase {
 
     Faker faker = new Faker();
     FakeValuesService fakeValuesService = new FakeValuesService(
@@ -45,25 +38,9 @@ public class CheckFormTests {
             city = "Agra",
             fileName = "1.png";
 
-    @BeforeAll
-    static void setUp() {
-        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-        Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
-    }
-
-    @AfterEach
-    @Step("Attachments")
-    public void afterEach() {
-        attachScreenshot("Last screenshot");
-        attachPageSource();
-        attachAsText("Browser console logs", getConsoleLogs());
-
-        closeWebDriver();
-    }
-
     @Test
-    public void fillForm() {
+    @DisplayName("Successful fill registration form")
+    public void successfulFillForm() {
 //        Заполнение и сохранение формы
         step("Открытие страницы с формой для заполнения.", ()-> open("https://demoqa.com/automation-practice-form"));
         step("Ввод имени и фамилии", () -> {
@@ -74,6 +51,87 @@ public class CheckFormTests {
             $("#userEmail").setValue(mail);
             $("#genterWrapper").$(byText(gender)).click();
             $("#userNumber").setValue(phoneNumber);
+        });
+        step("Ввод даты рождения.", ()-> {
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__year-select").selectOption(year);
+            $(".react-datepicker__month-select").selectOption(month);
+            $x("//div[@class='react-datepicker__month']//div[contains(text(),'" + date + "')]").click();
+        });
+        step ("Выбор изучаемых предметов.", ()-> {
+            $("#subjectsInput").setValue(subject1).pressEnter();
+            $("#subjectsInput").setValue(subject2).pressEnter();
+        });
+        step("Выбор хобби", ()-> {
+            $("#hobbiesWrapper").$(byText(hobby1)).click();
+            $("#hobbiesWrapper").$(byText(hobby2)).click();
+            $("#hobbiesWrapper").$(byText(hobby3)).click();
+        });
+        step("Загрузка файла картинки.", ()-> {
+            $("#uploadPicture").uploadFile(new File("src/test/resources/" + fileName));
+        });
+        step("Внесение адреса, штата и города.", ()-> {
+            $("#currentAddress").setValue(currentAddress);
+            $x("//div[@id='state']//input").setValue(state).pressEnter();
+            $x("//div[@id='city']//input").setValue(city).pressEnter();
+        });
+        step("Обращение к кнопке сохранения формы.", ()-> $("#submit").click());
+        step("Проверка отображения формы просмотра внесенных значений.", ()-> {
+            $x("//div[@id='example-modal-sizes-title-lg']").shouldHave(Condition.text("Thanks for submitting the form"));
+        });
+        step("Проверка имени и фамилии", ()-> {
+            $x("//tbody//td[contains(text(),'Student Name')]/../td[2]").shouldHave(Condition.text(firstName));
+            $x("//tbody//td[contains(text(),'Student Name')]/../td[2]").shouldHave(Condition.text(lastName));
+        });
+        step("Проверка почты", ()-> {
+            $x("//tbody//td[contains(text(),'Student Email')]/../td[2]").shouldHave(Condition.text(mail));
+        });
+        step("Проверка пола", ()-> {
+            $x("//tbody//td[contains(text(),'Gender')]/../td[2]").shouldHave(Condition.text(gender));
+        });
+        step("Проверка номера телефона", ()-> {
+            $x("//tbody//td[contains(text(),'Mobile')]/../td[2]").shouldHave(Condition.text(phoneNumber));
+        });
+        step("Проверка даты рождения", ()-> {
+            $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text(date));
+            $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text(month));
+            $x("//tbody//td[contains(text(),'Date of Birth')]/../td[2]").shouldHave(Condition.text(year));
+        });
+        step("Проверка предметов", ()-> {
+            $x("//tbody//td[contains(text(),'Subjects')]/../td[2]").shouldHave(Condition.text(subject1));
+            $x("//tbody//td[contains(text(),'Subjects')]/../td[2]").shouldHave(Condition.text(subject2));
+        });
+        step("Проверка выбранных хобби", ()-> {
+            $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text(hobby1));
+            $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text(hobby2));
+            $x("//tbody//td[contains(text(),'Hobbies')]/../td[2]").shouldHave(Condition.text(hobby3));
+        });
+        step("Проверка на наличие имени прикрепленного файла", ()-> {
+            $x("//tbody//td[contains(text(),'Picture')]/../td[2]").shouldHave(Condition.text(fileName));
+        });
+        step("Проверка адреса", ()-> {
+            $x("//tbody//td[contains(text(),'Address')]/../td[2]").shouldHave(Condition.text(currentAddress));
+        });
+        step("Проверка штата и города", ()-> {
+            $x("//tbody//td[contains(text(),'State and City')]/../td[2]").shouldHave(Condition.text(state));
+            $x("//tbody//td[contains(text(),'State and City')]/../td[2]").shouldHave(Condition.text(city));
+        });
+
+    }
+
+    @Test
+    @DisplayName("Unsuccessful fill registration form")
+    public void unsuccessfulFillForm() {
+//        Заполнение и сохранение формы
+        step("Открытие страницы с формой для заполнения.", ()-> open("https://demoqa.com/automation-practice-form"));
+        step("Ввод имени и фамилии", () -> {
+            $("#firstName").setValue(firstName);
+            $("#lastName").setValue(lastName);
+        });
+        step("Ввод почтыб пола и номера телефона", ()-> {
+            $("#userEmail").setValue(mail);
+            $("#genterWrapper").$(byText(gender)).click();
+            $("#userNumber").setValue(fakeValuesService.regexify("[0-9]{9}")); //short telephone number
         });
         step("Ввод даты рождения.", ()-> {
             $("#dateOfBirthInput").click();
