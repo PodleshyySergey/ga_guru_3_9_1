7e3db2e40adc9c3188996bd0703f84d1da8970ba
PodlesnyySergey <serpodl@gmail.com>
2020-12-18 23:40:42 +0300

    Comment out Configuration.remote for Selenoid. Add class TestBase.

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,30 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.AttachmentsHelper.*;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.startMaximized = true;
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
+    }
+
+    @AfterEach
+    @Step("Attachments")
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+
+        closeWebDriver();
+    }
+}
